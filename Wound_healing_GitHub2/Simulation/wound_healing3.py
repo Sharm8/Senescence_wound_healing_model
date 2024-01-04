66f701a3a5fd36b834d6a262126d18868eaa4df0
SC888 <43683199+Sharm8@users.noreply.github.com>
2024-01-04 10:37:53 +0000

    Add files via upload

---
+++ Wound_healing_GitHub2/Simulation/wound_healing3.py
@@ -0,0 +1,33 @@
+
+from cc3d import CompuCellSetup
+        
+
+from wound_healing3Steppables import MechanismsSteppable
+CompuCellSetup.register_steppable(steppable=MechanismsSteppable(frequency=1))
+       
+        
+from wound_healing3Steppables import PlotsSteppable
+CompuCellSetup.register_steppable(steppable=PlotsSteppable(frequency=1))
+     
+  
+from wound_healing3Steppables import ClearSteppable
+CompuCellSetup.register_steppable(steppable=ClearSteppable(frequency=1))
+
+      
+from wound_healing3Steppables import WoundSteppable
+CompuCellSetup.register_steppable(steppable=WoundSteppable(frequency=1))
+
+        
+from wound_healing3Steppables import MitosisSteppable
+CompuCellSetup.register_steppable(steppable=MitosisSteppable(frequency=1))
+     
+        
+from wound_healing3Steppables import SecretionSteppable
+CompuCellSetup.register_steppable(steppable=SecretionSteppable(frequency=1))
+
+
+#from wound_healing3Steppables import update_workspace_dirSteppable
+#CompuCellSetup.register_steppable(steppable=update_workspace_dirSteppable(frequency=100))
+       
+
+CompuCellSetup.run()
